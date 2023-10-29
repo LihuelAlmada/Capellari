28c4edcd5ca73b550aa81f98f37d88b15ad18562
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-10-28 21:57:15 -0300

    add PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Electrodomesticos Capellari',
   description: 'Los mejores electrodomesticos del mercado',
+  keywords: ['e-commerce', 'appliances'],
+  openGraph: {
+    title: 'Electrodomésticos Capellari',
+    description: 'Un viaje hacia la transformación',
+    type: 'website',
+  },
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon.png",
+  },
+  themeColor: "#000000"
 }
 
 export default function RootLayout({
