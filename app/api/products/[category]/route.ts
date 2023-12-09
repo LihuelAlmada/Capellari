0611c701b15a33eaf9a93a897b7641aaa17a17c4
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-12-09 16:16:32 -0300

    using database

--- app/api/products/[category]/route.ts
+++ app/api/products/[category]/route.ts
@@ -1,22 +1,32 @@
-import { mockData } from "@/app/lib/products-data"
-import { NextResponse } from "next/server"
-import { NextApiRequest } from "next/types/index"
-
+import { NextResponse } from "next/server";
+import { NextApiRequest } from "next/types/index";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/firebase/config";
 
 type Props = {
   params: { category: string };
 };
 
-const sleep = async (timer : Number ) => new Promise((resolve) => setTimeout(resolve))
+const sleep = async (timer: Number) =>
+  new Promise((resolve) => setTimeout(resolve));
 
-export const GET = async (_:NextApiRequest, { params } : Props ) => {
+export const GET = async (_: NextApiRequest, { params }: Props) => {
   const { category } = params;
-    
-    const items = category === 'all'
-                    ? mockData
-                    : mockData.filter(product => product.type === category)
 
-    await sleep(1000)
+  const productRef = collection(db, "products");
 
-    return NextResponse.json(items)
-}
+  const q =
+    category === "all"
+      ? productRef
+      : query(productRef, where("type", "==", category));
+
+  const querySnapshot = await getDocs(q);
+
+  const docs = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  await sleep(1000);
+
+  return NextResponse.json(docs);
+};
